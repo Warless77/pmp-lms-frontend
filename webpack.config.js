ab645e948d12bb3dbdeca89ef89765725c11f9d6
Warless77 <kanthiparajahmanoharan@gmail.com>
2026-08-12 20:38:50 +1200

    fix: expose Supabase build-time configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const supabaseUrl = process.env.SUPABASE_URL || process.env.REACT_APP_SUPABASE_URL || '';
+const supabasePublishableKey = process.env.SUPABASE_PUBLISHABLE_KEY || process.env.REACT_APP_SUPABASE_ANON_KEY || '';
 
 module.exports = {
   mode: 'production',
@@ -17,34 +21,25 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+          options: { presets: ['@babel/preset-env', '@babel/preset-react'] }
         }
       },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: 'asset/resource'
-      }
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.(png|jpe?g|gif|svg)$/i, type: 'asset/resource' }
     ]
   },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
+  resolve: { extensions: ['.js', '.jsx'] },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html'
-    })
+    new webpack.DefinePlugin({
+      'process.env.SUPABASE_URL': JSON.stringify(supabaseUrl),
+      'process.env.SUPABASE_PUBLISHABLE_KEY': JSON.stringify(supabasePublishableKey),
+      'process.env.REACT_APP_SUPABASE_URL': JSON.stringify(supabaseUrl),
+      'process.env.REACT_APP_SUPABASE_ANON_KEY': JSON.stringify(supabasePublishableKey)
+    }),
+    new HtmlWebpackPlugin({ template: './public/index.html', filename: 'index.html' })
   ],
   devServer: {
-    static: {
-      directory: path.join(__dirname, 'public')
-    },
+    static: { directory: path.join(__dirname, 'public') },
     historyApiFallback: true,
     compress: true,
     port: 3000,
