f815eab72640e1a0bf013302c33aee6aa9622ed7
Warless77 <kanthiparajahmanoharan@gmail.com>
2026-08-13 20:05:39 +1200

    fix: mount learner entitlement provider

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
+import { EntitlementProvider } from './context/EntitlementContext.jsx';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <AuthProvider><App /></AuthProvider>
+      <AuthProvider>
+        <EntitlementProvider>
+          <App />
+        </EntitlementProvider>
+      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
